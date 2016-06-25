040433002913fc7984f122deb6bea069c43a75f7
Jakub Jarosz <jakub.jarosz@postpro.net>
2016-06-25 08:44:16 +0100

    lookup for lat and lng

--- hotels.py
+++ hotels.py
@@ -2,63 +2,44 @@
 
 from pprint import pprint
 import os
+import sys
 import csv
 import requests
+import requests_cache
 
 
-APIKEY = os.environ["GOOGLE_API_KEY"]
-GOOGLE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+API_KEY = os.environ['GOOGLE_API_KEY']
+requests_cache.install_cache(cache_name='googleapi_cache', backend='sqlite', expire_after=None)
 
 
 def make_lookup_phrase(row):
+    """Return full name and address for google geo api text search."""
     add1, add2 = map(row.get, ('Premises Name', 'Address'))
     address_text = "{} {}".format(add1, add2)
     return address_text
 
 
-def construct_row(row):
+def getlatlng(address):
+    """Return lat lng of the requested address."""
+    parameters = {'key': API_KEY, 'query': address}
+    base = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+    try:
+        response = requests.get(base, params=parameters)
+        answer = response.json()
+        lat_lng = answer['results'][0]['geometry']['location']
+    except IndexError:
+        return {'lat': '', 'lng': ''}
+    return lat_lng
+
+
+def build_row(row, latlng):
     registration_number = row.get('Registration Number')
-    premises_name = row.get('Premises Name')
+    name = row.get('Premises Name')
     address = row.get('Address')
     classification = row.get('Classification')
-    rooms = row.get('Total Number Rooms')
-
-    row_out = {
-        'Registration Number': registation_number,
-        'Premises Name': premisses_name,
-        'Address': address,
-        'Classification': classification,
-        'Total Number Rooms': rooms
-    }
-
-    return row_out
-
-
-def send_geo_request(phrase, url=GOOGLE_URL, api_key=API_KEY):
-    params = {'key': api_key, 'query': phrase}
-    res = requests.get(url, params=params)
-    if res.status
-    return res.json()
-
-
-def extract_data(res):
-    item = res.get('results')[0]
-    name = item.get('name')
-    address = item.get('formatted_address')
-    lat = item.get('geometry').get('location').get('lat')
-    lng = item.get('geometry').get('location').get('lng')
-    d_out = {'Name': name, 'Address': address, 'lat': lat, 'lng': lng}
-    return d_out
-
-
-def construct_row_from_google_response(row, extdata):
-    registration_number = row.get('Registration Number')
-    name = extdata.get('Name')
-    address = extdata.get('Address')
-    classification = row.get('Classification')
     number_of_rooms = row.get('Total Number Rooms')
-    lat = extdata.get('lat')
-    lng = extdata.get('lng')
+    lat = latlng.get('lat')
+    lng = latlng.get('lng')
 
     row_out = {
         'Registration Number': registration_number,
@@ -73,42 +54,40 @@ def construct_row_from_google_response(row, extdata):
     return row_out
 
 
-
 def transform(file_in, file_out):
-    with open('geohotels.csv', 'rb') as csvfile:
+    """Creates a new csv file with added lat lng columns."""
+    with open(file_in, 'rb') as csvfile:
         hotel_reader = csv.DictReader(csvfile, delimiter=',')
 
-        with open('google_hotels.csv', 'wb') as csv_out:
-            fieldnames = ['Registration Number', 'Premises Name', 'Address', 'Classification', 'Total Number Rooms', 'Lat', 'Lng']
-            hotel_writer = csv.DictWriter(csv_out, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
+        with open(file_out, 'wb') as csv_out:
+            fieldnames = [
+                'Registration Number', 'Premises Name',
+                'Address', 'Classification',
+                'Total Number Rooms', 'Lat', 'Lng'
+            ]
+            hotel_writer = csv.DictWriter(csv_out,
+                                          fieldnames=fieldnames,
+                                          quoting=csv.QUOTE_MINIMAL)
             hotel_writer.writeheader()
 
-
             for row in hotel_reader:
-                lookup_phrase = make_lookup_phrase(row)
-                print("Google lookup phrase: {}".format(lookup_phrase))
-                r = send_geo_request(lookup_phrase)
-                gdata = extract_data(r)
-                new_row = construct_row_from_google_response(row, gdata)
+                address = make_lookup_phrase(row)
+                print("Google lookup phrase: {}".format(address))
+                latlng = getlatlng(address)       # request to google geo text search
+                new_row = build_row(row, latlng)
                 hotel_writer.writerow(new_row)
 
 
-def main():
-    """
-    1. open incsv
-    2. for each row:
-        - try to get google
-    """ 
-    pass
-
-
-def send_request(address):
-    pass
+def main(argv):
+    transform(sys.argv[1], sys.argv[2])
 
 
 if __name__=="__main__":
-    #if len(sys.argv < 3):
-    #    sys.exit("Usage: {} <sanitized_file_in.csv>, <file_out.csv>".format(sys.argv[0]))
-    #
-    #sys.exit(main(sys.argv[1], sys.argv[2]))
-    send_request()
+    if len(sys.argv) < 3:
+        sys.exit("Usage: {} <sanitized_file_in.csv>, <file_out.csv>".format(sys.argv[0]))
+
+    if not os.path.exists(sys.argv[1]):
+        sys.exit("Error: {} file was not found!".format(sys.argv[1]))
+
+    sys.exit(main(sys.argv))
+
